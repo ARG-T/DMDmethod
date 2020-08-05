7ef621e7bc203584fe80d8a78ae43dc89f6290ce
ARG-T <toshiki117@icloud.com>
2020-08-06 01:43:53 +0900

    add free energy

--- DMD.py
+++ DMD.py
@@ -4,10 +4,16 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import const
 
 
 # DMD Class
 class DMDmethod:
+
+    # const
+    const.BOLTZMANN_CONSTANT = 1.380649 * (10**(-23))
+    const.PI = math.pi
+    const.DIRACS_CONSTANT = 1.054571817 * (10**(-34))
 
     # init
     # lat_x --> x軸の格子点の数
@@ -18,10 +24,15 @@ class DMDmethod:
         self.lat_z = lat_z
         self.atom_number = self.lat_x * self.lat_y * self.lat_z
         self.temperature = temperature
-        self.mass = mass
+        # 質量のlist
+        self.mass = np.zeros(self.atom_number, dtype = float)
+        # X_i(3方向)
         self.x_position = np.zeros(self.atom_number, dtype = float)
         self.y_position = np.zeros(self.atom_number, dtype = float)
         self.z_position = np.zeros(self.atom_number, dtype = float)
+        # alpha_i
+        self.alpha = np.zeros(self.atom_number, dtype = float)
+        # c_i
         self.occupancy = np.zeros(self.atom_number, dtype = float)
     
     # 初期状態の入力
@@ -31,26 +42,49 @@ class DMDmethod:
             for iy in range(self.lat_y):
                 for iz in range(self.lat_z):
                     i += 1
+                    self.mass[i] = random.random()
                     self.x_position[i] = self.lat_parameter*ix
                     self.y_position[i] = self.lat_parameter*iy
                     self.z_position[i] = self.lat_parameter*iz
+                    self.alpha[i] = random.random()
                     self.occupancy[i] = random.random()
     
     #  free_energy (reference:(8))
-    def DMD_free_energy(self, alpha, mean_pos, concentration):
-        # 第一項 (クーロンポテンシャル項)
-        F_DMD_first = 0
-        # 第二項 (埋め込みエネルギー項)
-        F_DMD_second = 0
-        # 第三項 (振動のエントロピー項)
-        F_DMD_third = 0
-        # 第四項 (混合のエントロピー項)
-        F_DMD_fourth = 0
-        return F_DMD_first + F_DMD_second + F_DMD_third + F_DMD_fourth
+    def DMD_free_energy(self):
+        free_energy = self.Coulomb_interaction() + self.electron_density_dependency() + self.vivrations_entropy() + self.mixed_entropy()
+        print(free_energy)
+        return free_energy
+
+    # クーロン相互作用
+    def Coulomb_interaction(self):
+        return 0
+    
+    # 電子密度依存項
+    def electron_density_dependency(self):
+        return 0
+    
+    # 振動のエントロピー項
+    # TODO:変数名がわかりにくいので修正
+    def vivrations_entropy(self):
+        constant = 3 * const.BOLTZMANN_CONSTANT * self.temperature
+        viv_ent_list = self.occupancy * (np.log(self.alpha*(self.thermal_wavelength()**2)/const.PI)-1)
+        return constant * np.sum(viv_ent_list)
+    
+    # 混合のエントロピー項
+    # TODO:変数名がわかりにくいので修正
+    def mixed_entropy(self):
+        constant = const.BOLTZMANN_CONSTANT * self.temperature
+        mix_ent_list = self.occupancy * np.log(self.occupancy) + (1-self.occupancy) * np.log(1-self.occupancy)
+        return constant * np.sum(mix_ent_list)
+    
+    # ドブロイ波長
+    def thermal_wavelength(self):
+        return const.DIRACS_CONSTANT * ((2*const.PI)/(self.mass*const.BOLTZMANN_CONSTANT*self.temperature))**(1/2)
     
     # メインループ
     def main_roop(self):
         self.initial_pos()
+        self.DMD_free_energy()
         self.sample_print()
 
     def sample_print(self):
