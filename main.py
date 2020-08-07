b68e35b21b54b1f6edfdc828fab8fd1c98d80f8e
ARG-T <toshiki117@icloud.com>
2020-08-07 21:10:41 +0900

    add structure

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # 実行ファイル
 import DMD
+import crystal_structure
 
 if __name__ == "__main__":
-    lat_parameter, lat_x, lat_y, lat_z, temperature, mass = map(int,input().split())
-    DMD_analysis = DMD.DMDmethod(lat_parameter, lat_x, lat_y, lat_z, temperature, mass)
+    lat_parameter, lat_x, lat_y, lat_z, temperature, mass = map(int, input().split())
+    structure = input()
+    atom_position = crystal_structure.det_structure(structure)
+    DMD_analysis = DMD.DMDmethod(lat_parameter, lat_x, lat_y, lat_z, atom_position, temperature, mass)
     DMD_analysis.main_roop()
