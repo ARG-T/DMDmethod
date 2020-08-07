de935a1334619872b8fbc77b40243c5c94bac5d3
ARG-T <toshiki117@icloud.com>
2020-08-08 01:37:16 +0900

    arrange code

--- const.py
+++ const.py
@@ -1,6 +1,8 @@
 """
 Constant types in Python.
 """
+import sys
+
 
 class _const:
     class ConstError(TypeError):
@@ -11,5 +13,5 @@ class _const:
             raise self.ConstError("Can't rebind const (%s)" % name)
         self.__dict__[name] = value
 
-import sys
-sys.modules[__name__]=_const()
+
+sys.modules[__name__] = _const()
