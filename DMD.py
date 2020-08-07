2900764585b8abaac643ca4a6cbbe4400f6b3e8d
ARG-T <toshiki117@icloud.com>
2020-08-08 01:31:13 +0900

    add neighbor_list

--- DMD.py
+++ DMD.py
@@ -16,12 +16,11 @@ class DMDmethod:
     const.DIRACS_CONSTANT = 1.054571817 * (10**(-34))
 
     # init
-    # lat_x --> x軸方向に格子がいくつあるか
-    # lat_x * lat_y * lat_z * (単位格子内の原子数)
-    def __init__(self, lat_parameter, lat_x, lat_y, lat_z, atom_position, temperature, mass):
+    def __init__(self, lat_parameter, lat_x, lat_y, lat_z, atom_position, temperature, mass, x_period, y_period, z_period):
         # 格子定数
         # TODO : 立方晶以外のにも対応できるようにする
         self.lat_parameter = lat_parameter
+        # lat_x --> x軸方向に格子がいくつあるか
         self.lat_x = lat_x
         self.lat_y = lat_y
         self.lat_z = lat_z
@@ -42,9 +41,22 @@ class DMDmethod:
         self.occupancy = []
         # 原子の個数(initial_pos時に決定)
         self.atom_number = 0
+        # ベルレリスト
+        self.Verlet_neighbor_list = []
+        # 参照用リスト
+        self.reference_neighbor_list = []
+        # カットオフ半径(r_cut)
+        self.cutoff_radius = 0
+        # リスト半径(r_list)
+        self.list_radius = 10.00000001
+        # 周期境界条件
+        self.x_period = x_period
+        self.y_period = y_period
+        self.z_period = z_period
 
     # 初期状態の入力
     def initial_pos(self):
+        i = 0
         for ix in range(self.lat_x+1):
             for iy in range(self.lat_y+1):
                 for iz in range(self.lat_z+1):
@@ -52,13 +64,20 @@ class DMDmethod:
                         x = self.lat_parameter*(ix+x_pos)
                         y = self.lat_parameter*(iy+y_pos)
                         z = self.lat_parameter*(iz+z_pos)
-                        # 範囲内に存在する
-                        if (0 <= x <= self.lat_parameter*self.lat_x and
-                            0 <= y <= self.lat_parameter*self.lat_y and
-                                0 <= z <= self.lat_parameter*self.lat_z):
+                        # 周期境界条件
+                        # 周期境界条件が満たされる場合(True)は格子を一つ加算したぎりぎりまで、
+                        # 満たされていない場合(False)は微小量を足す(不等号を用いるので)
+                        x_add = 1.0 if self.x_period else 1e-9
+                        y_add = 1.0 if self.y_period else 1e-9
+                        z_add = 1.0 if self.z_period else 1e-9
+                        if (0 <= x < self.lat_parameter*(self.lat_x+x_add) and
+                            0 <= y < self.lat_parameter*(self.lat_y+y_add) and
+                                0 <= z < self.lat_parameter*(self.lat_z+z_add)):
                             self.x_position.append(x)
                             self.y_position.append(y)
                             self.z_position.append(z)
+                            print(i, (x, y, z))
+                            i += 1
                             self.mass.append(random.random())
                             self.alpha.append(random.random())
                             self.occupancy.append(random.random())
@@ -69,6 +88,39 @@ class DMDmethod:
         self.alpha = np.array(self.alpha, dtype=float)
         self.occupancy = np.array(self.occupancy, dtype=float)
         self.atom_number = len(self.mass)
+        self.Verlet_neighbor_list = [0] * (self.atom_number)
+        self.reference_neighbor_list = [0] * (self.atom_number**2)
+
+    # 二原子間の距離
+    def two_atoms_distance(self, i, j):
+        dx = abs(self.x_position[i]-self.x_position[j])
+        dy = abs(self.y_position[i]-self.y_position[j])
+        dz = abs(self.z_position[i]-self.z_position[j])
+        return (dx, dy, dz)
+
+    # 近接リスト作成
+    # 周期境界条件(~period)
+    def make_Verlet_neighbor_list(self):
+        nlist = 0
+        for i in range(self.atom_number):
+            self.Verlet_neighbor_list[i] = nlist+1
+            for j in range(i+1, self.atom_number):
+                dx, dy, dz = self.two_atoms_distance(i, j)
+                # x方向の周期境界条件が適用されている
+                if self.x_period:
+                    dx = min(dx, self.lat_parameter*(self.lat_x+1)-dx)
+                if self.y_period:
+                    dy = min(dy, self.lat_parameter*(self.lat_y+1)-dy)
+                if self.z_period:
+                    dz = min(dz, self.lat_parameter*(self.lat_z+1)-dz)
+                dr = dx**2+dy**2+dz**2
+                if dr < (self.list_radius)**2:
+                    nlist += 1
+                    self.reference_neighbor_list[nlist] = j
+        self.Verlet_neighbor_list[self.atom_number-1] = nlist+1
+        # デバッグ用
+        # print(self.Verlet_neighbor_list)
+        # print(self.reference_neighbor_list)
 
     #  free_energy (reference:(8))
     def DMD_free_energy(self):
@@ -107,6 +159,7 @@ class DMDmethod:
     def main_roop(self):
         self.initial_pos()
         self.DMD_free_energy()
+        self.make_Verlet_neighbor_list()
         self.sample_print()
 
     def sample_print(self):
