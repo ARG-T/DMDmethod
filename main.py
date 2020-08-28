4500cf13298b85f2364c6389b6447b0821f4bdd9
ARG-T <toshiki117@icloud.com>
2020-08-28 15:33:54 +0900

    support csv

--- main.py
+++ main.py
@@ -2,10 +2,5 @@
 import DMD
 
 if __name__ == "__main__":
-    lat_parameter, lat_x, lat_y, lat_z, temperature, mass, structure, x_period, y_period, z_period = config.input_info()
-    atom_position = crystal_structure.det_structure(structure)
-    if atom_position is False:
-        print("Error !")
-        exit()
-    DMD_analysis = DMD.DMDmethod(lat_parameter, lat_x, lat_y, lat_z, atom_position, temperature, mass, x_period, y_period, z_period)
+    DMD_analysis = DMD.DMDmethod()
     DMD_analysis.main_roop()
