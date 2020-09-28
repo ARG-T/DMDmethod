6ed44f1c94bdc47fd1dcf9a3315200b14014eda4
ARG-T <toshiki117@icloud.com>
2020-09-28 11:19:32 +0900

    arrange code

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
+"""
+sourceの名前を一致させる!!!
+"""
 
-import numpy as np
-
-sourcefiles = ['compute.pyx']
 setup(
-    cmdclass = {'build_ext':build_ext},
-    ext_modules = [Extension('compute_sample', sourcefiles)],
-    include_dirs = [np.get_include()]
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [
+                    Extension("c_inte_test", sources=["c_inte_test.pyx"], include_dirs=[numpy.get_include()],)
+                    ]
 )
