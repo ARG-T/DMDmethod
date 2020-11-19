514530f36fca667843999498625beef3bbc63f3d
ARG-T <toshiki117@icloud.com>
2020-11-19 13:16:24 +0900

    culc lat.

--- Cython_main.py
+++ Cython_main.py
@@ -1,9 +1,27 @@
 from c_free_energy import FreeEnergy
-import time
+
+
+def culc_lat(temperature):
+    print(temperature)
+    lat_p = 3.65
+    temperature = temperature
+    sigma = pow(10, -5)
+    learning_rate = 0.1
+    while True:
+        FE1 = FreeEnergy(lat_p+sigma, temperature)
+        FE2 = FreeEnergy(lat_p-sigma, temperature)
+        free1 = FE1.main_loop()
+        free2 = FE2.main_loop()
+        slope = (free1-free2)/(2*sigma)
+        delta = slope*learning_rate
+        if abs(delta) < pow(10, -7):
+            return lat_p, temperature
+            break
+        if abs(delta) < pow(10, -6):
+            learning_rate = 0.05
+        lat_p -= delta
 
 
 if __name__ == "__main__":
-    s = time.time()
-    FE = FreeEnergy()
-    FE.main_loop()
-    print("time", time.time()-s)
+    result = culc_lat(200)
+    print(result)
